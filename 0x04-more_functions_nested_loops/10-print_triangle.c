96d1f2c45311492223fa437620beb413009f5587
Tsholofelo Kolwane <tsholok8@gmail.com>
2023-03-24 04:21:28 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: The size of the squares triangle
+ * Return: empty
+ */
+
+void print_triangle(int size)
+{
+	int x;
+	int y;
+	int z;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < size; x++)
+	{
+	for (y = size - x; y > 1; y--)
+	{
+	_putchar(32);
+	}
+	for (z = 0; z <= x; z++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
+}
