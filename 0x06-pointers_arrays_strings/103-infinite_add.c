21d89aad04789eae824a02acc98de5a4c44ca5ec
Tsholofelo Kolwane <tsholok8@gmail.com>
2023-03-31 12:41:31 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -25,9 +25,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index)
 
 {
+
 	int num, tens = 0;
 
 	for (; *n1 && *n2; n1--, n2--, r_index--)
+
 	{
 	num = (*n1 - '0') + (*n2 - '0');
 	num += tens;
@@ -45,13 +47,15 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 
 	for (; *n2; n2--, r_index--)
 	{
+
 	num = (*n2 - '0') + tens;
 	*(r + r_index) = (num % 10) + '0';
 	tens = num / 10;
 	}
-	
+
 	if (tens && r_index >= 0)
 	{
+
 	*(r + r_index) = (tens % 10) + '0';
 	return (r + r_index);
 	}
@@ -80,10 +84,13 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
 {
+
 	int index, n1_len = 0, n2_len = 0;
-	
+
 	for (index = 0; *(n1 + index); index++)
+
 	n1_len++;
 
 	for (index = 0; *(n2 + index); index++)
@@ -97,4 +104,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	*(r + size_r) = '\0';
 
 	return (add_strings(n1, n2, r, --size_r));
+
 }
