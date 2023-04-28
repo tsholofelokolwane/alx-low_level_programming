1195f8b5ab64ccfb67343f017d1e7be3a8e8475c
Tsholofelo Kolwane <tsholok8@gmail.com>
2023-04-28 07:17:15 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * list_len: number of elements in linked list
+ * @h: head of node
+ *
+ * Return: count of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	int cnt = 0;
+	const list_t *current_node = h;
+
+	while (current_node != NULL)
+	{
+		cnt++;
+		current_node = current_node->next;
+	}
+
+	return (cnt);
+}
