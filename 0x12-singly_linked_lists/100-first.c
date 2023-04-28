1bc79ebb96ba6b7448b624d5223b52cc273533c0
Tsholofelo Kolwane <tsholok8@gmail.com>
2023-04-28 15:53:06 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * myStartupFun - prints before the main function is executed
+ *
+ */
+
+/* implementation of myStartupFun */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
