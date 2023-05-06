ef24aae65f66d963128f9ddfd3f3386ada0ea058
Tsholofelo Kolwane <tsholok8@gmail.com>
2023-05-06 15:10:07 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c;
+
+	c = (char *) &i;
+	if (*c == 1)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
