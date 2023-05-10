7747542453daa4f45af20a7ba4a4a1b2eb3fe01a
Tsholofelo Kolwane <tsholok8@gmail.com>
2023-05-10 04:46:44 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of the file to create
+ * @text_content: a NULL terminated string to
+ * write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ * if the file already exists, truncate it
+ * if filename is NULL return -1
+ * if text_content is NULL create an empty file
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	FILE *fp;
+	size_t len = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fp = fopen(filename, "w");
+	if (fp == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		fwrite(text_content, sizeof(char), len, fp);
+	}
+
+	fclose(fp);
+
+	return (1);
+}
